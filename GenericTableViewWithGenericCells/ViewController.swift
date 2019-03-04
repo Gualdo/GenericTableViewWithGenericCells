2e21084f333015ebfb15df8ad81ffd754d88821b
Eduardo De La Cruz <gualdodelacruz@gmail.com>
2019-03-04 15:40:33 +0100

    Initial Commit

---
+++ GenericTableViewWithGenericCells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericTableViewWithGenericCells
+//
+//  Created by De La Cruz, Eduardo on 04/03/2019.
+//  Copyright © 2019 De La Cruz, Eduardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
